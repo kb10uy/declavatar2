e01477ae5bec166d37b44f052eef5a823eab9072
Yū KOBAYASHI <kb10uy@kb10uy.org>
2026-09-15 14:14:37 +0900

    Support bit width, add provided parameters

--- declavatar2/src/vrchat/expr_parameter.rs
+++ declavatar2/src/vrchat/expr_parameter.rs
@@ -14,43 +14,118 @@ pub struct ExpressionParameter {
     pub synced: bool,
 }
 
-/// Represents type and default value (if exist) of a `ExpressionParameter`.
+/// Represents type and default value (if exist) of a [`ExpressionParameter`]s.
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum ExpressionParameterTypeDefault {
     /// Boolean.
     Bool(Option<bool>),
 
     /// Integer.
-    Int(Option<u8>),
+    Int { width: ExpressionParameterWidth, default: Option<u8> },
 
     /// Float \[-1.0, 1.0\].
-    Float(Option<f32>),
+    Float { width: ExpressionParameterWidth, default: Option<f32> },
+}
+
+/// Represents [`ExpressionParameter`] bit width for Int/Float parameters.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ExpressionParameterWidth {
+    /// The width is not specified by the script.
+    /// Actual width will be determined from its usage.
+    Unspecified,
+
+    /// The width is specified by the script.
+    Specified(u8),
 }
 
 impl ExpressionParameterTypeDefault {
     /// Bit width of this type.
-    pub fn data_width(&self) -> usize {
+    pub fn data_width(&self) -> Option<u8> {
         match self {
-            ExpressionParameterTypeDefault::Bool(_) => 1,
-            ExpressionParameterTypeDefault::Int(_) => 8,
-            ExpressionParameterTypeDefault::Float(_) => 8,
+            ExpressionParameterTypeDefault::Bool(_) => Some(1),
+            ExpressionParameterTypeDefault::Int { width, .. } | ExpressionParameterTypeDefault::Float { width, .. } => match width {
+                ExpressionParameterWidth::Unspecified => None,
+                ExpressionParameterWidth::Specified(w) => Some(*w),
+            },
         }
     }
 
     pub fn animated_value_type(&self) -> AnimatedValueType {
         match self {
             ExpressionParameterTypeDefault::Bool(_) => AnimatedValueType::Bool,
-            ExpressionParameterTypeDefault::Int(_) => AnimatedValueType::Int,
-            ExpressionParameterTypeDefault::Float(_) => AnimatedValueType::Float,
+            ExpressionParameterTypeDefault::Int { .. } => AnimatedValueType::Int,
+            ExpressionParameterTypeDefault::Float { .. } => AnimatedValueType::Float,
         }
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum VrchatProvidedParameter {}
+pub enum VrchatProvidedParameter {
+    IsLocal,
+    PreviewMode,
+    Viseme,
+    Voice,
+    GestureLeft,
+    GestureRight,
+    GestureLeftWeight,
+    GestureRightWeight,
+    AngularY,
+    VelocityX,
+    VelocityY,
+    VelocityZ,
+    VelocityMagnitude,
+    Upright,
+    Grounded,
+    Seated,
+    Afk,
+    TrackingType,
+    VrMode,
+    MuteSelf,
+    InStation,
+    Earmuffs,
+    IsOnFriendsList,
+    AvatarVersion,
+    IsAnimtorEnabled,
+    ScaleModified,
+    ScaleFactor,
+    ScaleFactorInverse,
+    EyeHeightAsMeters,
+    EyeHeightAsPercent,
+}
 
 impl VrchatProvidedParameter {
     pub fn animated_value_type(&self) -> AnimatedValueType {
-        match self {}
+        match self {
+            VrchatProvidedParameter::IsLocal => AnimatedValueType::Bool,
+            VrchatProvidedParameter::PreviewMode => AnimatedValueType::Int,
+            VrchatProvidedParameter::Viseme => AnimatedValueType::Int,
+            VrchatProvidedParameter::Voice => AnimatedValueType::Float,
+            VrchatProvidedParameter::GestureLeft => AnimatedValueType::Int,
+            VrchatProvidedParameter::GestureRight => AnimatedValueType::Int,
+            VrchatProvidedParameter::GestureLeftWeight => AnimatedValueType::Float,
+            VrchatProvidedParameter::GestureRightWeight => AnimatedValueType::Float,
+            VrchatProvidedParameter::AngularY => AnimatedValueType::Float,
+            VrchatProvidedParameter::VelocityX => AnimatedValueType::Float,
+            VrchatProvidedParameter::VelocityY => AnimatedValueType::Float,
+            VrchatProvidedParameter::VelocityZ => AnimatedValueType::Float,
+            VrchatProvidedParameter::VelocityMagnitude => AnimatedValueType::Float,
+            VrchatProvidedParameter::Upright => AnimatedValueType::Float,
+            VrchatProvidedParameter::Grounded => AnimatedValueType::Bool,
+            VrchatProvidedParameter::Seated => AnimatedValueType::Bool,
+            VrchatProvidedParameter::Afk => AnimatedValueType::Bool,
+            VrchatProvidedParameter::TrackingType => AnimatedValueType::Int,
+            VrchatProvidedParameter::VrMode => AnimatedValueType::Int,
+            VrchatProvidedParameter::MuteSelf => AnimatedValueType::Bool,
+            VrchatProvidedParameter::InStation => AnimatedValueType::Bool,
+            VrchatProvidedParameter::Earmuffs => AnimatedValueType::Bool,
+            VrchatProvidedParameter::IsOnFriendsList => AnimatedValueType::Bool,
+            VrchatProvidedParameter::AvatarVersion => AnimatedValueType::Int,
+            VrchatProvidedParameter::IsAnimtorEnabled => AnimatedValueType::Bool,
+            VrchatProvidedParameter::ScaleModified => AnimatedValueType::Bool,
+            VrchatProvidedParameter::ScaleFactor => AnimatedValueType::Float,
+            VrchatProvidedParameter::ScaleFactorInverse => AnimatedValueType::Float,
+            VrchatProvidedParameter::EyeHeightAsMeters => AnimatedValueType::Float,
+            VrchatProvidedParameter::EyeHeightAsPercent => AnimatedValueType::Float,
+        }
     }
 }
